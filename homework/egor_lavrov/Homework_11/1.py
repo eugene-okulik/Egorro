9b3ef63649040a4334db2d3bb24c12e4689f3495
Egorro <egorro@inbox.ru>
2025-02-28 11:16:53 +0300

    Lint fixes

--- homework/egor_lavrov/Homework_11/1.py
+++ homework/egor_lavrov/Homework_11/1.py
@@ -1,6 +1,5 @@
 class Book:
 
-
     def __init__(self, title, author, number_of_pages, page_material, ISBN, presence_of_text, reserved):
         self.title = title
         self.author = author
@@ -9,7 +8,6 @@ class Book:
         self.ISBN = ISBN
         self.presence_of_text = presence_of_text
         self.reserved = reserved
-
 
     def info(self):
         print(f'Название: {self.title}\n'
@@ -23,18 +21,14 @@ class Book:
             print('')  # Для отделения информации между книгами
 
 
-
-
 class SchoolBook(Book):
 
-
     def __init__(self, title, author, number_of_pages, page_material, ISBN, presence_of_text, reserved,
-        subject, grade, presence_of_assignment):
+            subject, grade, presence_of_assignment):
         super().__init__(title, author, number_of_pages, page_material, ISBN, presence_of_text, reserved)
         self.subject = subject
         self.grade = grade
         self.presence_of_assignment = presence_of_assignment
-
 
     def info(self):
         print(f'Название: {self.title}\n'
@@ -43,7 +37,7 @@ class SchoolBook(Book):
               f'Предмет: {self.subject}\n'
               f'Класс: {self.grade}')
         if self.reserved:
-            print(f'Зарезервирована\n')
+            print('Зарезервирована\n')
         else:
             print('')  # Для отделения информации между книгами
 
@@ -54,10 +48,10 @@ DFM_02 = Book('Униженные и оскорблённые', 'Достоев�
               'Офсетная бумага', '978-5-17-135181-6', True, False)
 PAS_01 = Book('Стихотворения. Поэмы', 'Пушкин Александр Сергеевич', '512',
               'Офсетная бумага', '978-5-04-102518-2', True, True)
-dSEA_01 = Book('Маленький принц и Цитадель','де Сент-Экзюпери Антуан', '488',
-               'Офсетная бумага', '978-5-04-173229-5', True, False)
-BIG_01 = Book('Что? Зачем? Почему?','Барановская Ирина Геннадьевна', '256',
-               'Мелованная бумага', '978-5-17-094259-6', True, False)
+dSEA_01 = Book('Маленький принц и Цитадель', 'де Сент-Экзюпери Антуан', '488',
+              'Офсетная бумага', '978-5-04-173229-5', True, False)
+BIG_01 = Book('Что? Зачем? Почему?', 'Барановская Ирина Геннадьевна', '256',
+              'Мелованная бумага', '978-5-17-094259-6', True, False)
 
 sb_NSM_05 = SchoolBook('Математика. 5 класс', 'Никольский Сергей Михайлович', '272',
                        'Офсетная бумага', '978-5-09-018849-4', True, True,
