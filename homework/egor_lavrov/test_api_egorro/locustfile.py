80d966621db695f75330ff6db0b93d5894c5cec6
Egorro <egorro@inbox.ru>
2026-04-15 00:26:34 +0300

    Lint fixes

--- homework/egor_lavrov/test_api_egorro/locustfile.py
+++ homework/egor_lavrov/test_api_egorro/locustfile.py
@@ -3,7 +3,6 @@ from locust import task, HttpUser
 
 class ObjLocust(HttpUser):
 
-
     @task(1)
     def get_all_objects(self):
         self.client.get('',
@@ -11,7 +10,7 @@ class ObjLocust(HttpUser):
 
     @task(5)
     def get_one_objects(self):
-        self.client.get(f'/1',
+        self.client.get('/1',
                         headers={"Content-Type": "application/json"})
 
     @task(1)
@@ -19,11 +18,11 @@ class ObjLocust(HttpUser):
         payload = {"name": "Apple_1", "data": {"color": "red", "size": "small"}}
         headers = {"Content-Type": "application/json"}
         response = self.client.post('',
-                         json=payload,
-                         headers=headers
-                         )
+                                    json=payload,
+                                    headers=headers
+                                    )
         obj_id = response.json()['id']
         self.client.delete(f'/{obj_id}',
-                         json=payload,
-                         headers=headers
-                         )
+                           json=payload,
+                           headers=headers
+                           )
