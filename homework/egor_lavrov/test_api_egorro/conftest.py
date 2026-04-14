e2e5c16d082ad9b0827e449eb3f327bcbb2bae5f
Egorro <egorro@inbox.ru>
2026-04-14 16:19:53 +0300

    Rewrite new_post_id fixsture

--- homework/egor_lavrov/test_api_egorro/conftest.py
+++ homework/egor_lavrov/test_api_egorro/conftest.py
@@ -33,14 +33,10 @@ def delete_object_endpoint():
 
 
 @pytest.fixture()
-def new_post_id():
-    body = {"name": "My object",
+def new_post_id(post_object_endpoint, delete_object_endpoint, get_object_endpoint):
+    payload = {"name": "My object",
             "data": {"color": "red",
                      "size": "medium"}}
-    headers = {"Content-Type": "application/json"}
-    response = requests.post('http://objapi.course.qa-practice.com/object',
-                             json=body,
-                             headers=headers)
-    post_id = response.json()['id']
+    post_id = post_object_endpoint.post_object(payload=payload).json()['id']
     yield post_id
-    requests.delete(f'http://objapi.course.qa-practice.com/object/{post_id}')
+    delete_object_endpoint.delete_object(post_id)
--- homework/egor_lavrov/test_api_egorro/endpoints/endpoint.py
+++ homework/egor_lavrov/test_api_egorro/endpoints/endpoint.py
@@ -8,23 +8,9 @@ class Endpoint:
     post_id = None
     headers = {"Content-Type": "application/json"}
 
-    # @allure.step('Create new object')
-    # def new_post_id(self):
-    #     payload = {"name": "My object",
-    #             "data": {"color": "red",
-    #                      "size": "medium"}}
-    #     self.response = requests.post(self.url,
-    #                                   json=payload,
-    #                                   headers=self.headers
-    #                                   )
-    #     self.json = self.response.json()
-    #     post_id = self.json()['id']
-    #     yield post_id
-    #     requests.delete(f'{self.url}/{post_id}')
 
     @allure.step('Check response status code')
     def assert_status_code_is(self, code):
-        print(f'{self.response.status_code} == {code}')
         assert self.response.status_code == code, 'Wrong status code'
 
     def assert_name_in_response_matches_payload(self, name):
